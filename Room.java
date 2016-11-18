f01fa0732795140814ed4b6e73fc4c9940c839fb
Adam Wadsworth <adamwads@btinternet.com>
2016-11-18 17:23:36 +0000

    started room class

---
+++ Room.java
@@ -0,0 +1,19 @@
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.util.*;
+import com.jogamp.opengl.util.awt.*;
+import com.jogamp.opengl.util.texture.*;
+import com.jogamp.opengl.util.texture.awt.*;
+
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT;
+
+public class Room {
+	
+	public Room() {
+
+	}
+
+	public drawRoom() {
+		
+	}
+}
